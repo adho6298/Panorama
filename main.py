67057403364b0cfe886f103b06578e185443545a
adho6298 <143643450+adho6298@users.noreply.github.com>
2025-09-11 15:06:19 -0600

    updating cause im working on it

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-from utils import *
+from utils import *         # Loading in all of our libraries and utility functions
 
-print(test(5))
+''' 
+Part A - Inputing Images
+'''
+
+''' Load images from the specified folder. ONLY UNCOMMENT ONE OF THE FOLLOWING LINES '''
+image_list = load_images_from_folder('Panorama/Images/VictoriaLibrary')
+# image_list = load_images_from_folder('Panorama/Images/Indoors')
+# image_list = load_images_from_folder('Panorama/Images/Flatirons')
+# image_list = load_images_from_folder('Panorama/Images/CULogo')
+# image_list = load_images_from_folder('Panorama/Images/CUBoulderSatView')
+# image_list = load_images_from_folder('Panorama/Images/Checkerboard')
+show_all_images(image_list, "Source Images")        # Displaying all of the source images
+
+'''Scaling down the images to make processing faster '''
+scale_images = [img.copy() for img in image_list]   # Creating a copy of the original list for processing
+scale = 0.6                                         # Scaling factor (0 < scale <= 1)
+scale_list = scale_img(image_list, scale)           # Scaling all of the images
+show_all_images(scale_list, "Scaled Images")        # Displaying all of the scaled images
+
+''' Converting to grayscale to make corner detection easier '''
+gray_images = [img.copy() for img in scale_list]    # Creating a copy of the scaled list for processing
+gray_images = convert_to_gray(gray_images)          # Converting all of the images to grayscale
+show_all_images(gray_images, "Gray Images")         # Displaying all of the gray images
+
+'''
+Part B - Detect Corners
+'''
--- utils.py
+++ utils.py
@@ -10,22 +10,57 @@ import os
 import glob
 
 # FUNCTIONS AND UTILITIES
-# Display Function for plotting an image using Matplotlib:
-def show_img(img):
-    plt.imshow(img)
-    plt.axis("off")
-    plt.show()
+# Display Function for plotting an image using cv2.imshow
+def show_img(img, window_name):
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    h, w = img.shape[:2]
+    cv2.resizeWindow(window_name, w, h)
+    cv2.imshow(window_name, img)
+
+
+def show_all_images(image_list, window_name):
+    """
+    Displays all images in the list in separate resizable windows.
+    """
+    for idx, img in enumerate(image_list):
+        show_img(img, window_name=f"{window_name} - Image {idx+1}")
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 # Function to scale an image by a given factor
-def scale_img(img, scale):
-    resized = cv2.resize(img, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
-    return resized
+def scale_img(image_list, scale):
+    """
+    Scales every image in the provided list by the given scaling factor.
+    Returns a new list of scaled images.
+    """
+    scaled_images = []
+    for img in image_list:
+        resized = cv2.resize(img, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
+        scaled_images.append(resized)
+    return scaled_images
 
 # Function to import an image from a given path
 def import_img(path):
     img = cv2.imread(path)
     return img
 
-def test(num):
-    sum = num + num
-    return sum
+def load_images_from_folder(folder_path):
+    image_list = []
+    extensions = ('*.jpg', '*.jpeg', '*.png', '*.bmp', '*.tiff')
+    for ext in extensions:
+        search_pattern = os.path.join(folder_path, ext)
+        print(f"Searching for: {search_pattern}")  # Debug print
+        for filename in glob.glob(search_pattern):
+            print(f"Found file: {filename}")  # Debug print
+            img = cv2.imread(filename)
+            if img is not None:
+                image_list.append(img)
+    return image_list
+
+def convert_to_gray(image_list):
+    gray_images = []
+    for img in image_list:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray_images.append(gray)
+    return gray_images
